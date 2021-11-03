d5f47fe540fda0337d7200f9676e275c20375017
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2021-11-03 11:02:33 +0700

    database

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -4,6 +4,9 @@ const CONFIG = {
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
   DEFAULT_LANGUAGE: 'en-us',
   CACHE_NAME: 'KemangiResto-V1',
+  DATABASE_NAME: 'kemang-resto-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
 }
 
 export default CONFIG
