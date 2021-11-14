feb81df34c31a5b4d3724e419f28ddb4fa71cb37
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2021-11-14 22:26:45 +0700

    footer finish

--- src/scripts/views/components/footer.js
+++ src/scripts/views/components/footer.js
@@ -1,19 +1,44 @@
 /* eslint-disable linebreak-style */
 class Footer extends HTMLElement {
   connectedCallback() {
-    this.render();
+    this.render()
   }
 
   render() {
     this.innerHTML = `
     <footer>
       <div class="container">
-        <h4>Kemangi resto</h4>
-        <p>Copyright © 2021 irfan-fauzi</p>
+        <div class="wrap sitemap">
+          <h3 class="title">sitemap</h3>
+          <ul class="flex">
+            <li><a href="#/">Beranda</a></li>
+            <li><a href="#/favorite">favorite</a></li>
+            <li><a href="#/about">About</a></li>
+          </ul>
+        </div>
+        <div class="wrap kontak">
+          <h3 class="title">contact us</h3>
+          <div class="flex">
+            <a href="#">
+              <img class="social-media" src="/icons/facebook.png" alt="facebook">
+            </a>
+            <a href="#">
+              <img class="social-media" src="/icons/twitter.png" alt="twitter">
+            </a>
+            <a href="#">
+              <img class="social-media" src="/icons/instagram.png" alt="instagram">
+            </a>
+          </div>
+        </div>
+        <div class="wrap penutup">
+          <img src="icons/logo-3.svg" alt="logo" class="logo">
+          <p>Kemangi adalah sebuah website untuk mendapatkan informasi restoran-restoran kekinian dan terlengkap.</p>
+          <p>Copyright © 2021 - Kemangi</p>
+        </div>
       </div>
     </footer>
-    `;
+    `
   }
 }
 
-customElements.define('app-footer', Footer);
+customElements.define('app-footer', Footer)
