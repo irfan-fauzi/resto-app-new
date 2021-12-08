b39dccda904cbe1a68a5c7293cb5df4318a79317
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2021-12-08 14:40:56 +0700

    like dan dislike test

---
+++ specs/removeFromFavoriteSpec.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-undef */
+import FavoriteRestoranIdb from '../src/scripts/data/favorite-resto-idb'
+import FavoriteButtonPresenter from '../src/scripts/utils/favoriteButtonPresenter'
+
+describe('Hapus data dari favorite', () => {
+  beforeEach(async () => {
+    document.body.innerHTML = '<div id="buttonContainer"></div>'
+    await FavoriteRestoranIdb.putResto({ id: 1 })
+  })
+  afterEach(async () => {
+    await FavoriteRestoranIdb.deleteResto(1)
+  })
+  const btnPresenterInit = async (resto) => {
+    await FavoriteButtonPresenter.init({
+      parentElement: document.querySelector('#buttonContainer'),
+      restoDetail: resto,
+      favoriteResto: FavoriteRestoranIdb,
+    })
+  }
+  // test 1
+  it('harusnya muncul tombol "hapus" ketika resto sudah difavoritkan', async () => {
+    await btnPresenterInit({ id: 1 })
+    expect(document.querySelector('[aria-label="remove this resto"]'))
+      .toBeTruthy()
+  })
+
+  // test 2
+  it('harusnya tidak muncul tombol "save" ketika resto sudah difavoritkan', async () => {
+    await btnPresenterInit({ id: 1 })
+    expect(document.querySelector('[aria-label="add this resto"]'))
+      .toBeFalsy()
+  })
+
+  // test 3
+  it('harusnya bisa menghapus resto dari daftar favorit', async () => {
+    await btnPresenterInit({ id: 1 })
+    document.querySelector('[aria-label="remove this resto"]').dispatchEvent(new Event('click'))
+    expect(await FavoriteRestoranIdb.getAllResto()).toEqual([])
+  })
+
+  // test 4
+  it('harusnya tidak ada error jika resto tidak ada dalam database', async () => {
+    // tambahkan data : {id: 1}
+    await btnPresenterInit({ id: 1 })
+    // hapus lagi data tadi
+    await FavoriteRestoranIdb.deleteResto(1)
+    document.querySelector('[aria-label="remove this resto"]').dispatchEvent(new Event('click'))
+    // hasilnya harus kosong
+    expect(await FavoriteRestoranIdb.getAllResto()).toEqual([])
+  })
+})
