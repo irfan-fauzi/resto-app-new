f099d642c965fbd949cd672548c32034abb286dd
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2021-12-10 14:20:29 +0700

    liking a resto

--- e2e/Liking_Resto.spec.js
+++ e2e/Liking_Resto.spec.js
@@ -1,6 +1,25 @@
 /* eslint-disable no-undef */
 Feature('Add Favorite Resto')
 
-Scenario('test something', ({ I }) => {
-  I.amOnPage()
+Before(({ I }) => {
+  I.amOnPage('/#/favorite')
+})
+
+Scenario('showing empty message', ({ I }) => {
+  I.seeElement('not-found')
+  I.see('belum ada restourant favorite', '.title')
+})
+
+Scenario('liking one resto', async ({ I }) => {
+  I.see('belum ada restourant favorite', '.title')
+  I.amOnPage('/')
+
+  I.seeElement('#title-resto a')
+  I.click(locate('#title-resto a').first())
+
+  I.seeElement('.btn-favorite')
+  I.click('.btn-favorite')
+
+  I.amOnPage('/#/favorite')
+  I.seeElement('.article__item')
 })
