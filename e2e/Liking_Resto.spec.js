98a1f2036436428ebee7e4bc4100df77a5d1fbe6
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2021-12-11 08:35:07 +0700

    test semua berhasil

--- e2e/Liking_Resto.spec.js
+++ e2e/Liking_Resto.spec.js
@@ -32,26 +32,46 @@ Scenario('liking one resto', async ({ I }) => {
   assert.strictEqual(favoriteResto, firstRestoTitle)
 })
 
-Scenario('liking 3 resto', async ({ I }) => {
+Scenario('liking two resto and make sure that resto show right', async ({ I }) => {
   I.see('belum ada restourant favorite', '#not-found-text')
   I.amOnPage('/')
 
   I.seeElement('#title-resto a')
   const titles = []
 
-  for (let i = 1; i <= 3; i++) {
+  for (let i = 1; i <= 2; i++) {
     const titleClicked = locate('#title-resto a').at(i)
     const valueTitle = await I.grabTextFrom(titleClicked)
-    // klik judul resto => otomatis akan ke halaman /favorite
     I.click(titleClicked)
-    // di halaman favorite, lihat dulu kontennya
     I.seeElement('.detail')
     I.seeElement('.btn-favorite')
     I.click('.btn-favorite')
     titles.push(valueTitle)
-    // kembali ke home
     I.amOnPage('/')
   }
   I.amOnPage('/#/favorite')
+  I.see(titles[0], '#title-resto a')
+  I.see(titles[1], '#title-resto a')
+})
+
+Scenario('add and than delete from favorite resto', async ({ I }) => {
+  I.see('belum ada restourant favorite', '#not-found-text')
+  I.amOnPage('/')
+
+  I.seeElement('#title-resto a')
+  I.click(locate('#title-resto a').first())
+
+  I.seeElement('.btn-favorite')
+  I.click('.btn-favorite')
+
+  I.amOnPage('/#/favorite')
   I.seeElement('.article__item')
+
+  I.seeElement('#title-resto a')
+  I.click(locate('#title-resto a').first())
+  I.seeElement('.btn-favorite')
+  I.click('.btn-favorite')
+
+  I.amOnPage('/#/favorite')
+  I.see('belum ada restourant favorite', '#not-found-text')
 })
