53dc27f12322ea16d2452fe430b9ccf5033dbd32
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2021-12-12 08:04:47 +0700

    sw

--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'src/scripts/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
   },
   module: {
     rules: [
