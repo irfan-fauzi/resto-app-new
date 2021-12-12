90c1cacc8b8d62ae723a30f710bf6f87b7209b2b
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2021-12-12 09:23:33 +0700

    send

--- src/scripts/views/components/not-found.js
+++ src/scripts/views/components/not-found.js
@@ -9,8 +9,8 @@ class NotFound extends HTMLElement {
       <h1 id="not-found-text" class="title text-center">belum ada restourant favorite</h1>
       <picture>
       <source srcset="images/food-truck.webp" type="image/webp">
-      <source srcset="images/food-truck.svg" type="image/jpeg">
-      <img src="images/food-truck.svg" alt="data kosong" class="img-ilustrasi">
+      <source srcset="images/food-truck.png" type="image/png">
+      <img src="images/food-truck.png" alt="data kosong" class="img-ilustrasi">
       </picture>
     </div>
     `
